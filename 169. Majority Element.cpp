a47d4a8451d6fed5ad5c582f00a23ee1e4ac7fe4
Saymamulla2604 <108901313+Saymamulla2604@users.noreply.github.com>
2023-11-07 12:01:52 +0530

    Create 169. Majority Element.cpp

---
+++ 169. Majority Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int res=0;
+        for(auto num: nums)
+        {
+            if(count==0)
+            res=num;
+            if(num!=res)
+            count--;
+            else
+            count++;
+        }
+        return res;
+    }
+};
